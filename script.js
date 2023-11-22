95e4e552211c8b7fd7023641703e714bd298cd93
ciciDawgy <108370999+ciciDawgy@users.noreply.github.com>
2023-11-22 13:26:32 -0500

    Create rocket.html

---
+++ script.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="rocket.css">
+    <title>Rocket</title>
+</head>
+<body>
+    <div class="scene">
+      <!--<div class="nums">
+        <div class="num5">5</div>
+        <div class="num4">4</div>
+        <div class="num3">3</div>
+        <div class="num2">2</div>
+        <div class="num1">1</div>
+      </div>-->
+      <div class="rocket">
+        <video src="fire.mp4" autoplay muted></video>
+        <img src="https://www.pngarts.com/files/4/Rocket-PNG-Photo.png" class="rocket">
+      </div>
+    </div>
+
+    <script src="rocket.js"></script>
+</body>
+</html>
